b007cf0210e1a8ac716c78804cdce7c19104cb59
Egor-Kozh <kozhevnikovegor55@gmail.com>
2023-07-16 19:09:57 +0000

    177711

--- spacebattle/Class1.cs
+++ spacebattle/Class1.cs
@@ -1,5 +1,21 @@
-﻿namespace cpasebattle;
-public class Class1
-{
+﻿using System;
 
+namespace spacebattle
+{
+    public class Class1
+    {
+        public static double[] ConstantMotion(double x, double y, double sp_x, double sp_y, double MotionExist){
+            double [] newPos = new double[2];
+            if (MotionExist == 0 || x == double.NaN ) 
+            {
+                throw new Exception();
+            }
+            else
+            {
+                newPos[0] = x + sp_x;
+                newPos[1] = y + sp_y;
+            }
+            return newPos;
+        }
+    }
 }
